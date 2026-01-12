098af8be7fee3b741bfa41d0807ffd8700ed44f2
Possawat Wongwikrom <ping5721@gmail.com>
2026-01-12 21:07:29 +0700

    feat: try swaggo

--- internal/domain/user/router.go
+++ internal/domain/user/router.go
@@ -2,20 +2,27 @@ package user
 
 import (
 	response "backend/internal/infrastructure/http"
-	"net/http"
-
 	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 func NewUserRouter(r *echo.Echo, userService *UserService) {
+
 	userGroup := r.Group("/users")
-	{
-		userGroup.GET("/", func(c echo.Context) error {
-			users, err := userService.getUsers(c.Request().Context())
-			if err != nil {
-				c.JSON(http.StatusNotFound, nil)
-			}
-			return response.Success(c, users)
-		})
-	}
+	
+	// GetUser godoc
+	// @Summary Get user
+	// @Tags user
+	// @Produce json
+	// @Param id path string true "User ID"
+	// @Success 200 {object} UserResponse
+	// @Failure 404 {object} map[string]string
+	// @Router /user/{id} [get]
+	userGroup.GET("/", func(c echo.Context) error {
+		users, err := userService.getUsers(c.Request().Context())
+		if err != nil {
+			return c.JSON(http.StatusNotFound, nil)
+		}
+		return response.Success(c, users)
+	})
 }
--- main.go
+++ main.go
@@ -3,14 +3,38 @@ package main
 import (
 	"backend/ent"
 	"backend/internal/domain/user"
-	"github.com/labstack/echo/v4"
 	"fmt"
-	_ "github.com/lib/pq"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
+	echoSwagger "github.com/swaggo/echo-swagger"
+	_ "github.com/swaggo/echo-swagger/example/docs"
 )
+
+//	@title			Swagger Example API
+//	@version		1.0
+//	@description	This is a sample server celler server.
+//	@termsOfService	http://swagger.io/terms/
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+//	@host		localhost:8080
+//	@BasePath	/api/
+
+//	@securityDefinitions.basic	NoAuth
+
+//	@externalDocs.description	OpenAPI
+//	@externalDocs.url			https://swagger.io/resources/open-api/
 
 func main() {
 	e := echo.New()
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	client, err := ent.Open("postgres", "postgres://f49e2bf29c4bc8a564588af0b6f7ae1c8e80da2d47a882855d65f55a382f7d8e:sk_t3l6zuh-8gG5tODdYEhEW@db.prisma.io:5432/postgres?sslmode=require")
 	if err != nil {
 		fmt.Println(err)
@@ -25,16 +49,16 @@ func main() {
 	// })
 	// defer rdb.Close()
 
-	e.GET("healthcheck",func(c echo.Context) error {
-		return c.JSON(http.StatusOK,"hihi")
+	e.GET("healthcheck", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "hihi")
 	})
 
 	if err != nil {
 		panic(e)
 	}
 
-	userRepository := user.UserRepository{Db:client}
-	userService := user.UserService{UserRepository:&userRepository}
+	userRepository := user.UserRepository{Db: client}
+	userService := user.UserService{UserRepository: &userRepository}
 	user.NewUserRouter(e, &userService)
 	e.Logger.Fatal(e.Start(":8080"))
 }
